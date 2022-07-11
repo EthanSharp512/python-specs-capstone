c4b6d6b889caac311a37839c3fac7e032517df81
EthanSharp512 <ethansharpdev@gmail.com>
2022-07-11 16:18:26 -0600

    genres db file made

--- model.py
+++ model.py
@@ -1,4 +1,8 @@
+import imp
+from os import environ
+
 from enum import auto, unique
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -25,3 +29,18 @@ class Genre(db.Model):
 
 class Subgenre(db.Model):
     pass
+
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ['URI']
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+if __name__ == "__main__":
+
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
---
+++ server.py
@@ -0,0 +1,26 @@
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, redirect, request, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+
+#from model import classes
+
+app = Flask(__name__)
+
+
+
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+    # make sure templates, etc. are not cached in debug mode
+    app.jinja_env.auto_reload = app.debug
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
