f41e232a56d7b54a0a330f0f7a6defa8cf2b55a8
EthanSharp512 <ethansharpdev@gmail.com>
2022-07-13 00:51:18 -0600

    set up forms.py with a form for creating an event

---
+++ forms.py
@@ -0,0 +1,14 @@
+from xmlrpc.client import Boolean
+from flask_wtf import FlaskForm
+from wtforms import StringField, IntegerField, SubmitField, DateField, BooleanField, SelectField
+
+class AddEventForm(FlaskForm):
+
+    event_name = StringField('Name of event: ')
+    artist = StringField('Name of artist: ')
+    location = StringField('Location of event: ')
+    event_date = DateField('Date of event: ')
+    public = BooleanField('Post event privately or publicly: ')
+    genre = SelectField('Select a genre: ')
+    subgenre = SelectField('Select sub-genre if applicable: ')
+    submit = SubmitField('Create Event')
--- server.py
+++ server.py
@@ -2,11 +2,11 @@
 from os import environ
 
 from jinja2 import StrictUndefined
-
-from flask import Flask, render_template, redirect, request, flash, session
+from flask import Flask, render_template, redirect, request, flash, session, url_for
 from flask_debugtoolbar import DebugToolbarExtension
 
 from model import User, Genre, Subgenre, Event, Post, connect_to_db, db
+from forms import AddEventForm
 
 app = Flask(__name__)
 
@@ -20,10 +20,35 @@ app.jinja_env.undefined = StrictUndefined
 def home():
     return render_template("home.html")
 
+@app.route('/new', methods=['GET', 'POST'])
+def new_event():
+        
+    form = AddEventForm()
 
+    if form.validate_on_submit():
 
+        event_name = form.event_name.data
+        artist = form.artist.data
+        location = form.location.data
+        event_date = form.event_date.data
+        public = form.public.data
+        genre = form.genre.data
+        subgenre = form.subgenre.data
+        
+        new_event = Event(event_name, artist, location, event_date, public, genre, subgenre)
 
+        db.session.add(new_event)
+        db.session.commit()
 
+        return redirect(url_for('profile'))
+
+    return render_template('new_event_form.html', form=form)
+
+@app.route('/profile')
+def my_profile():
+
+    events = Event.query.all()
+    return render_template('profile.html', events=events)
 
 
 if __name__ == "__main__":
